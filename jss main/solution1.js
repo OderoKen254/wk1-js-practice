7b00469a517595ad6953813816d2d7b63fe5e994
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-04-01 14:14:19 +0300

    add grade calculator function

---
+++ jss main/solution1.js
@@ -0,0 +1,36 @@
+
+    function calcGrade() {
+        const marks = parseInt(prompt("Enter the student marks (0 - 100):"), 10);
+    
+        if (marks < 0 || marks > 100 || isNaN(marks)) {
+            console.log("Invalid input! Please enter a number between 0 and 100.");
+        } else if (marks > 79) {
+            console.log("Grade: A");
+        } else if (marks >= 60 && marks <= 79) {
+            console.log("Grade: B");
+        } else if (marks >= 50 && marks <= 59) {
+            console.log("Grade: C");
+        } else if (marks >= 40 && marks <= 49) {
+            console.log("Grade: D");
+        } else {
+            console.log("Grade: E");
+        }
+    }
+
+const marks = parseInt(prompt("Enter the student marks (0 - 100):"), 10);
+    
+        if (marks < 0 || marks > 100 || isNaN(marks)) {
+            console.log("Invalid input! Please enter a number between 0 and 100.");
+        } else if (marks > 79) {
+            console.log("Grade: A");
+        } else if (marks >= 60 && marks <= 79) {
+            console.log("Grade: B");
+        } else if (marks >= 50 && marks <= 59) {
+            console.log("Grade: C");
+        } else if (marks >= 40 && marks <= 49) {
+            console.log("Grade: D");
+        } else {
+            console.log("Grade: E");
+        }
+
+        calcGrade();
